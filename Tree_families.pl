47249dbc601275cfad1557e6aa1ef4b8f12dc187
DenisMalyshev27 <s0163511@edu.kubsu.ru>
2024-03-28 20:05:52 +0300

    Lr 1. Zad 1. Added unifications

--- Tree_families.pl
+++ Tree_families.pl
@@ -53,43 +53,26 @@ men():- man(X), print(X), nl, fail.
 women():- woman(X), print(X), nl, fail.
 children(X):- parent(X,Y), print(Y), nl, fail.
 
+%Инициализирован предикат mother(X,Y), который проверяет, является ли X матерью Y.
+%mother(+X,+Y)
 mother(X,Y):- woman(X), parent(X,Y).
+%Инициализирован предикат mother(X), который выводит всех матерей X.
+%mother(+X)
 mother(X):- mother(Y,X), print(Y), nl, fail.
 
 %Инициализирован предикат brother(X,Y), который проверяет, является ли X братом Y.
+%brother(+X,+Y)
 brother(X,Y):-man(X), parent(Z,X), parent(Z,Y), man(Z).
 %Инициализирован предикат brothers(X), который выводит всех братьев X.
+%brothers(+X)
 brothers(X) :- brother(Y,X),print(Y), nl, fail.
 
 %Инициализирован предикат b_s(X,Y), который проверяет, являются ли X и Y родными братом и сестрой или братьями или сестрами.
+%b_s(+X,+Y)
 b_s(X,Y) :- parent(Z,X), parent(Z,Y), man(Z), X \= Y.
 %Инициализирован предикат b_s(X), который выводит всех братьев или сестер X.
+%b_s(+X)
 b_s(X):- b_s(Y,X),parent(Z,Y),parent(Z,X),man(Z),print(Y), nl, fail.
-
-%Инициализирован предикат father(X, Y), который проверяет, является ли X отцом Y.
-father(X,Y):-man(X), parent(X,Y).
-%Инициализирован предикат father(X), который выводит отца X.
-father(X):-father(Y,X),print(Y),nl,fail.
-
-%Инициализирован предикат sister(X, Y), который проверяет, является ли X сестрой Y.
-sister(X,Y):-woman(X),parent(Z,X),parent(Z,Y),man(Z), X \= Y.
-%Инициализирован предикат sisters(X), который выводит всех сестер X.
-sisters(X):-sister(Y,X),print(Y),nl,fail.
-
-%Инициализирован предикат grand_so(X, Y), который проверяет, является ли X внуком Y.
-grand_so(X,Y):-parent(Y,Z),parent(Z,X),man(X).
-%Инициализирован предикат grand_sons(X), который выводит всех внуков X.
-grand_sons(X):-grand_so(Y,X),print(Y),nl,fail.
-
-%Инициализирован предикат grand_pa_and_son(X,Y), который проверяет, являются ли X и Y дедушкой и внуком
-grand_pa_and_son(X,Y):-man(X),man(Y),parent(X,Z),parent(Z,Y).
-%Инициализирован предикат grand_pa_and_son(X,Y), который проверяет, являются ли X и Y внуком и дедушкой
-grand_pa_and_son(X,Y):-man(X),man(Y),parent(Z,X),parent(Y,Z).
-
-%Инициализирован предикат nephew(X,Y), который проверяет, является ли X племянником Y.
-nephew(X,Y):-parent(Z,X),(brother(Z,Y);sister(Z,Y)),man(X), \+ parent(Y,X).
-%Инициализирован предикат nephew(X), который выводит всех племянников X.
-nephew(X):-nephew(Y,X),print(Y),nl,fail.
 
 max(X,Y,Z):-(
      X>=Y -> Z is X;
